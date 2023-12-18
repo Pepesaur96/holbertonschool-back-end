41b4d79d8dabb2cc82da15f5ebe961c78e4289b9
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-18 12:43:44 -0400

    task0.trying other code

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,52 +3,20 @@
 import requests
 import sys
 
+if len(sys.argv) > 1 and sys.argv[1].isdigit():
+    user_id = sys.argv[1]
+    user = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)).json()
+    todos = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)).json()
 
-def get_employee_todo_progress(employee_id):
-    # Define the base URL of the REST API
-    base_url = "https://jsonplaceholder.typicode.com"
+    done_tasks = [task for task in todos if task.get('completed') is True]
+    total_tasks = len(todos)
 
-    # Construct the URL for the employee's TODO list
-    todo_url = f"{base_url}/todos?userId={employee_id}"
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user['name'], len(done_tasks), total_tasks))
 
-    try:
-        # Make a GET request to the API
-        response = requests.get(todo_url)
-
-        # Check if the request was successful (status code 200)
-        if response.status_code == 200:
-            # Parse the JSON response
-            todos = response.json()
-
-            # Get the employee's name
-            employee_name = todos[0]['username'] if todos else "Unknown"
-
-            # Count the number of completed tasks
-            completed_tasks = [todo for todo in todos if todo['completed']]
-            num_completed_tasks = len(completed_tasks)
-
-            # Display employee information
-            print(
-                f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{len(todos)}):")
-
-            # Display titles of completed tasks
-            for task in completed_tasks:
-                print(f"\t{task['title']}")
-        else:
-            print(
-                f"Error: Unable to fetch TODO list. Status code: {response.status_code}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-
-if __name__ == "__main__":
-    # Check if the script is provided with an employee ID as a command-line argument
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <employee_id>")
-        sys.exit(1)
-
-    # Get the employee ID from the command line
-    employee_id = int(sys.argv[1])
-
-    # Call the function to get and display employee TODO list progress
-    get_employee_todo_progress(employee_id)
+    for task in done_tasks:
+        print('\t {}'.format(task['title']))
+else:
+    print("Usage: {} <employee_id>".format(sys.argv[0]))
