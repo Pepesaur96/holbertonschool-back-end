f76662d32f99d9602c15bb4ad44a7f7a370c579a
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-19 18:16:37 -0400

    task0. added spacing to documentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -5,22 +5,31 @@ import sys
 
 # Get ID from command line
 id = sys.argv[1]
+
 # Get user name
 user = requests.get('https://jsonplaceholder.typicode.com/users/' + id)
+
 # Convert to JSON
 data = user.json()
+
 # Get name
 name = data['name']
+
 # Get todos
 todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + id)
+
 # Convert to JSON
 todos_data = todos.json()
+
 # Get total number of todos
 total = str(len(todos_data))
+
 # Get number of completed todos
 completed = str(sum([1 for task in todos_data if task['completed'] is True]))
+
 # Print Employee 'name' is done with tasks('completed'/'total'):
 print('Employee {} is done with tasks({}/{}):'.format(name, completed, total))
+
 # Print title of completed tasks
 for task in todos_data:
     if task['completed'] is True:
