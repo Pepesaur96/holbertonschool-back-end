a82125c99493693e1ec9899c6b2d2f7b4c2eab2e
Pepesaur96 <jjvazquez96@gmail.com>
2023-12-19 18:31:32 -0400

    task0. copied Jesus documentation

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """
-This script returns information about a given employee's TODO list progress.
-Arguments:
-    id - employee ID
-Returns:
-    The employee's name, the total number of tasks, and the number of tasks
-    completed out of the total. Then, the titles of all completed tasks.
+This module uses Python to make requests to a REST API.
+fix
+It fetches data about a specific employee's tasks
+and prints a summary of the tasks completed and the
+titles of the completed tasks.
 """
 import requests
 import sys
